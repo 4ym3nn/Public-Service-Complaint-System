ba773f4c0fdc76d2bcbe3dc64fea5b7b8ca2a924
Aymen-drid <aymnehocam@gmail.com>
2025-10-19 12:14:27 +0100

    change to production

--- complaint-system/admin/settings.py
+++ complaint-system/admin/settings.py
@@ -12,21 +12,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# The secret key
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ykpcb2ss8)w&wukp5_nr_u9yic&47#bc2#!s0!z@9231-kby@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
